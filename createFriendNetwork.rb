5b20dee26fb78745d3dcce453c171cf8e1b9c3ab
Derek Frankhouser <derek.frankhouser@gmail.com>
2014-05-13 11:03:34 -0400

    Added back in ElasticSearch.  Removed parrel processing to allow for less traffic to facebook graph.  Posts to index are now 3 per relationship. 1 in relationship and 2 in relationship_all for querying.

--- createFriendNetwork.rb
+++ createFriendNetwork.rb
@@ -43,35 +43,96 @@ end
 def pullProfilePic()
 end
 
+def persistRelationship(id1 , id2 , un1 , un2)
+  host = "http://localhost:9200/"
+  index = "facebookesp"
+  type = "relationship"
+  type_all = "relationship_all"
+  
+  id = ""
+  if id1.to_i < id2.to_i
+    id = "#{id1}#{id2}"
+  else
+    id = "#{id2}#{id1}"
+  end
+  id_all = "#{id1}#{id2}"
+
+  json = {"target"=>un1,"friend"=>un2}
+  json = json.to_json
+  
+  #post to condensed relationship type
+  url = "#{host}#{index}/#{type}/#{id}"
+  response = RestClient.post url , json, :content_type => :json, :accept => :json
+  
+  #post to relationship_all type
+  url = "#{host}#{index}/#{type_all}/#{id_all}"
+  response = RestClient.post url , json, :content_type => :json, :accept => :json
+  
+  #post opposite relationship to relationship_all 
+  json = {"target"=>un2,"friend"=>un1}
+  json = json.to_json
+  id_all = "#{id2}#{id1}"
+
+  url = "#{host}#{index}/#{type_all}/#{id_all}"
+  response = RestClient.post url , json, :content_type => :json, :accept => :json
+  
+end
 
 
 #TODO move to ARGV
 $accesstoken = ARGV[1]
 $host = "https://graph.facebook.com/v1.0/"
 
-# TODO change to list of profiles from CSV
+
+# STEP 1a: READ CSV FILE.  Check each row for fb_un.  IF un exists, check if fb_id field is set, if not, query graph to get id
 users = []
-CSV.foreach(ARGV[0], :headers => true) do |csv_obj|
-   if(csv_obj["fb_id"].nil? == false)
-      x = {"un" => csv_obj["fb_id"] , "id" => getProfileInfo(csv_obj["fb_id"],"id")}
-      puts x
-      users << x
+allusers = []
+puts "Step 1a >>> Read input file and add all Facebook ID #s where the fb_id field is missing or nil"  
+CSV.foreach(ARGV[0], :headers => true , :converters => []) do |csv_obj|
+csv_obj = csv_obj.to_hash
+   if(csv_obj["fb_un"].nil? == false)
+      if(csv_obj.has_key?("fb_id") == false || ( csv_obj.has_key?("fb_id") && (csv_obj["fb_id"].nil? || csv_obj["fb_id"] == false )) )
+          puts "Missing FB ID for #{csv_obj["fb_un"]}"
+          csv_obj["fb_id"] = getProfileInfo(csv_obj["fb_un"],"id");
+      end
+      users << csv_obj
    end
 end
 
+# STEP 1b:  Output new CSV to input_expand.csv.
+puts "Step 1b >>> Output new inputfile to input_expanded.csv"  
+CSV.open("input_expanded.csv","w") do |csv|
+  first = true
+  users.each do |x|
+      if(first)
+        first = false
+        csv << x.keys
+      end
+      csv << x.values
+  end
+end
+
+#STEP 2: Determine  all relationships between users.
+puts "Step 2 >>> Determine Relationships and store in array.  Post to ElasticSearch"
 relations = []
-users.peach(10) do |u|
-    users.peach(10) do |t|
-      if(getRelationship(u["id"],t["id"]))
-        puts "Relationship found!"
-        relations << { "targ" => u , "frnd" => t }
+users.each_with_index do | u , k1|
+    users.each_with_index do |t , k2|
+      if(k2 > k1)
+        if(getRelationship(u["fb_id"],t["fb_id"]))
+          puts "Relationship found! #{u["fb_un"]} - #{t["fb_un"]}"
+          persistRelationship(u["fb_id"],t["fb_id"] ,u["fb_un"],t["fb_un"])
+          relations << { "targ" => u , "frnd" => t }
+        end
       end
     end
 end
 
+puts "Step 3 >>> Persist relationships to output csv.  Each friendship saved once"
 CSV.open("relationships.csv","w") do |csv|
   csv << ["targ","frnd"]
   relations.each do |x|
-    csv << [x["targ"]["un"],x["frnd"]["un"]]
+    csv << [x["targ"]["fb_un"],x["frnd"]["fb_un"]]
   end
 end
+
+puts "Script complete."
